cdd8952bdf60e17d34fb408441a50e880abf287b
Gabriel Henrique <gh.deoliveira@outlook.com>
2023-05-11 20:16:53 -0300

    commit

--- Ex_Observer/src/exobserver/WeatherData.java
+++ Ex_Observer/src/exobserver/WeatherData.java
@@ -38,8 +38,7 @@ public class WeatherData implements Subject {
 		notifyObservers();
 	}
 	
-	public void setMeasurements(float temperature, float humidity,
-		float pressure){
+	public void setMeasurements(float temperature, float humidity, float pressure){
 		this.temperature = temperature;
 		this.humidity = humidity;
 		this.pressure = pressure;
